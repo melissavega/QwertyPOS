a0306ad0ae29922c906e3eb5ca33e5f0099ceda5
Asim K <akarowaliya@hotmail.com>
2019-04-02 00:04:34 -0500

    Added all the things that went missing in Register page when git hub failed

--- QwertyPOS/Register.aspx.cs
+++ QwertyPOS/Register.aspx.cs
@@ -16,23 +16,22 @@ namespace QwertyPOS
         {
 
         }
-
-        protected void btnSignup_Click(object sender, EventArgs e)
+        protected void btnSignup_Click1(object sender, EventArgs e)
         {
-            if(tbID.Text!="" && tbFirstName.Text!="" && tbLastName.Text!="" && tbPhoneNum.Text!="" && tbPassword.Text!="" && tbConfirmPass.Text != "")
+            if (tbID.Text != "" && tbFirstName.Text != "" && tbLastName.Text != "" && tbPhoneNum.Text != "" && tbPassword.Text != "" && tbConfirmPass.Text != "")
             {
-                if(tbPassword.Text == tbConfirmPass.Text)
+                if (tbPassword.Text == tbConfirmPass.Text)
                 {
-                 string CS = ConfigurationManager.ConnectionStrings["POS_SystemConnectionString2"].ConnectionString;
-                 using(SqlConnection con = new SqlConnection(CS))
-                {
-                SqlCommand cmd = new SqlCommand("INSERT INTO Users(ID_Number, FirstName, LastName, PhoneNumber, passsword) values('"+tbID.Text+"', '"+tbFirstName.Text+"', '"+tbLastName.Text+"', '"+tbPhoneNum.Text+"', '"+tbPassword.Text+"')", con);
-                con.Open();
-                cmd.ExecuteNonQuery();
+                    string CS = ConfigurationManager.ConnectionStrings["POS_SystemConnectionString2"].ConnectionString;
+                    using (SqlConnection con = new SqlConnection(CS))
+                    {
+                        SqlCommand cmd = new SqlCommand("INSERT INTO Users(ID_Number, FirstName, LastName, PhoneNumber, passsword) values('" + tbID.Text + "', '" + tbFirstName.Text + "', '" + tbLastName.Text + "', '" + tbPhoneNum.Text + "', '" + tbPassword.Text + "')", con);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
                         lblMsg.Text = "Registeration Successfull";
                         lblMsg.ForeColor = Color.Green;
                         Response.Redirect("Login.aspx");
-                }
+                    }
 
                 }
 
@@ -47,7 +46,6 @@ namespace QwertyPOS
                 lblMsg.ForeColor = Color.Red;
                 lblMsg.Text = "All fields are mandotary";
             }
-
         }
     }
 }
